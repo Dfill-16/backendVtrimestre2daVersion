cbdc5297ea0d08859e5b0e7fe442b5dda9c8751c
Dfill-16 <davidaso26@gmail.com>
2025-09-08 17:07:54 -0500

    Add: conexión a cluster de mongo db

--- src/libs/mongoose.js
+++ src/libs/mongoose.js
@@ -1,7 +1,7 @@
 const mongoose = require("mongoose");
 
-const URI = "mongodb://localhost:27017/blogs"
-
+// const URI = "mongodb://localhost:27017/blogs"
+const URI = "mongodb+srv://davidaso26:Dalawi1123@firstcluster.uqkbqwh.mongodb.net/?retryWrites=true&w=majority&appName=FirstCluster"
 
 async function Connection() {
   try {
