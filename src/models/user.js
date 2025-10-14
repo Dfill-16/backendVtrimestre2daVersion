897a301c5509bb87d623fd26fc287d448b8fc19f
Dfill-16 <davidaso26@gmail.com>
2025-10-14 16:49:21 -0500

    Add: model and scripts personalize

---
+++ src/models/user.js
@@ -0,0 +1,56 @@
+class User {
+  #id = null;
+  #name = null;
+  #email = null;
+  #password = null;
+
+  constructor(id = null, name = null, email = null, password = null) {
+    this.#id = id;
+    this.#name = name;
+    this.#email = email;
+    this.#password = password;
+  }
+
+  getid() {
+    return this.#id;
+  }
+
+  setid(value) {
+    this.#id = value;
+  }
+
+  getname() {
+    return this.#name;
+  }
+
+  setname(value) {
+    this.#name = value;
+  }
+
+  getemail() {
+    return this.#email;
+  }
+
+  setemail(value) {
+    this.#email = value;
+  }
+
+  getpassword() {
+    return this.#password;
+  }
+
+  setpassword(value) {
+    this.#password = value;
+  }
+
+  getValues() {
+    return {
+      id: this.#id,
+      name: this.#name,
+      email: this.#email,
+      password: this.#password,
+    };
+  }
+}
+
+module.exports = User;
